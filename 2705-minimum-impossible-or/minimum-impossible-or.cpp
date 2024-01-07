6afd3ff9fd63d753a004b9affc8ed9fdc50907ad
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2024-01-07 15:18:38 +0530

    Time: 172 ms (34.41%) | Memory: 74.9 MB (49.46%) - LeetSync

--- 2705-minimum-impossible-or/minimum-impossible-or.cpp
+++ 2705-minimum-impossible-or/minimum-impossible-or.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int minImpossibleOR(vector<int>& nums) {
-        int mx = nums[0], n = nums.size();
-        for(int i = 1; i < nums.size(); i++)if(mx<nums[i])mx=nums[i];
-        int k = floor(log2(mx));
+        int n = nums.size();
+
         unordered_map<int,int>mp;
         for(int i = 0 ; i < n ; i++)mp[nums[i]]+=1;
         int i = 0;
@@ -14,7 +13,5 @@ public:
             }
             i+=1;
         }
-        
-        
     }
 };

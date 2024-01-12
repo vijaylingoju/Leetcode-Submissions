518c401c093a4432851cbfeaf0e746a8dc7b0ca0
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2024-01-12 18:55:26 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (72.68%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
@@ -5,7 +5,7 @@ public:
         transform(s.begin(), s.end(), s.begin(), ::tolower); 
         for(int i = 0 ; i < n; i++){
             if(s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u'){
-                cout<<c<<" ";
+                // cout<<c<<" ";
                 if(i<n/2)c+=1;
                 else c-=1;
             }

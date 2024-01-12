8bdd1ad475df8032c28ac800b480eb2cd26513ea
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2024-01-12 18:53:47 +0530

    Time: 19 ms (5.01%) | Memory: 6.9 MB (96.85%) - LeetSync

---
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+        int n = s.size(), c = 0;
+        for(int i = 0 ; i < n; i++){
+            if(s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u' or 
+            s[i]=='A' or s[i]=='E' or s[i]=='I' or s[i]=='O' or s[i]=='U'){
+                cout<<c<<" ";
+                if(i<n/2)c+=1;
+                else c-=1;
+            }
+        }
+        
+        return c==0;
+    }
+};

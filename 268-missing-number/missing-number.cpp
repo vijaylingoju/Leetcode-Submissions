6d7f899faaf187b45a8fc6bcafb2123569799363
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2024-02-20 17:16:00 +0530

    Time: 6 ms (98.51%) | Memory: 20.4 MB (18.88%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -3,8 +3,8 @@ public:
     int missingNumber(vector<int>& nums) {
         ios_base::sync_with_stdio(false); 
         cin.tie(NULL);
-        int n = nums.size();
-        int x=accumulate(nums.begin(),nums.end(),0);
-        return (n*(n+1)/2 - x);
+        int n = nums.size(), sum = n*(n+1)/2;
+        int x=sum - accumulate(nums.begin(),nums.end(),0);
+        return x;
     }
 };

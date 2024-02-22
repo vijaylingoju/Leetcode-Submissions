8e3c300a2d446152d1d0f05d802da6f75c51574c
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2024-02-22 18:08:52 +0530

    Time: 123 ms (16.13%) | Memory: 52.1 MB (8.71%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
-    int fun(int i, vector<int>&nums, int n, int k, vector<vector<int>>&dp){
+    int fun(int i, vector<int>&nums, int n, int k, vector<vector<int>>&dp){ios_base::sync_with_stdio(false); 
+        cin.tie(NULL);
         if(k==0)return 0;
         if(i>=n)return 1e9;
-        int pick = INT_MAX;
+        int pick = 1e9;
         if(dp[i][k]!=-1)return dp[i][k];
         if(k>=nums[i]) pick = 1 + fun(i,nums,n,k-nums[i],dp);
         int nopick = fun(i+1,nums,n,k,dp);

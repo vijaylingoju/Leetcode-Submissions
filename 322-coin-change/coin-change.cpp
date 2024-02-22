960fd84d1c423038d4cf424cf6162a6cb9ef94d4
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2024-02-22 18:06:43 +0530

    Time: 59 ms (45.99%) | Memory: 52 MB (8.71%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int fun(int i, vector<int>&nums, int n, int k, vector<vector<int>>&dp){
+        if(k==0)return 0;
+        if(i>=n)return 1e9;
+        int pick = INT_MAX;
+        if(dp[i][k]!=-1)return dp[i][k];
+        if(k>=nums[i]) pick = 1 + fun(i,nums,n,k-nums[i],dp);
+        int nopick = fun(i+1,nums,n,k,dp);
+        return dp[i][k]=min(pick,nopick);
+    }
+    int coinChange(vector<int>& nums, int k) {
+        vector<vector<int>>dp(nums.size()+1,vector<int>(k+1,-1));
+        int ans = fun(0,nums,nums.size(),k,dp);
+        return ans==1e9 ? -1 : ans;
+    }
+};

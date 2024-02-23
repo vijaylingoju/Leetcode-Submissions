67b16fe2997611884ccbf199e8e88aded319f6a0
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2024-02-23 20:29:14 +0530

    Time: 23 ms (29.45%) | Memory: 17.3 MB (16.97%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<pair<int,int>>adj[n];
-        for(auto it:flights){
-            adj[it[0]].push_back({it[1],it[2]});
-        }
-        queue<pair<int,pair<int,int>>>q;
-        q.push({0,{src,0}});
-        vector<int>dist(n,1e9);
-        dist[src]=0;
+    int dp[101][101]; // Assuming n <= 100
+    
+    int fun(int i, int dst, int k, unordered_map<int,vector<pair<int,int>>>& adj, vector<int>& vis) {
+        //base cases
+        if (k < 0) return 1e9;
+        if ( i == dst) return 0;
         
-        while(!q.empty()){
-            auto it = q.front();
-            q.pop();
-            int x = it.first;
-            int i = it.second.first;
-            int cost = it.second.second;
-            if(x>k)continue;
-            for(auto it:adj[i]){
-                int j = it.first;
-                int z = it.second;
-                if(cost + z < dist[j] and x<=k){
-                    dist[j] = cost + z;
-                    q.push({x+1,{j,cost+z}});
-                }
+        if (dp[i][k] != -1) return dp[i][k]; 
+        
+        //vis[i] = 1;
+        int mi = 1e9;
+        for (auto pr : adj[i]) {
+            if (vis[pr.first] == 0) {
+                int cost = pr.second + fun(pr.first, dst, k - 1, adj, vis);
+                if(cost==1e9)continue;
+                mi = min(mi, cost);
             }
         }
+        //vis[i] = 0;
+        return dp[i][k] = mi; 
+    }
+    
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        memset(dp, -1, sizeof(dp));
+        unordered_map<int,vector<pair<int,int>>> adj;
+        for (auto it : flights) {
+            adj[it[0]].push_back({it[1], it[2]});
+        }
+    
         
-        if(dist[dst]==1e9)return -1;
-        
-        return dist[dst];
+        vector<int> vis(n, 0);
+        int check = fun(src, dst, k + 1, adj, vis);
+        return check == 1e9 ? -1 : check;
     }
 };

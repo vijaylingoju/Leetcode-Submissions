43f68a5b5c60044a3216e163034a906f97a2372b
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2024-06-09 15:56:37 +0530

    Time: 16 ms (10.79%) | Memory: 16.4 MB (51.99%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,23 +3,38 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<int>r(n,0);
-        vector<int>c(m,0);
-        
-        for(int i = 0; i < n; i++){
-            for(int j = 0 ; j < m; j++){
+        int cola = 1;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0; j < m; j++){
                 if(matrix[i][j]==0){
-                    r[i]=1;
-                    c[j]=1;
+                    matrix[i][0]=0;
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }
+                    else{
+                        cola = 0;
+                    }
                 }
             }
         }
-        
-        for(int i = 0; i < n; i++){
-            for(int j = 0 ; j < m; j++){
-                if(r[i] || c[j]){
-                    matrix[i][j]=0;
+        for(int i = 1 ; i < n ; i++){
+            for(int j = 1; j < m; j++){
+                if(matrix[i][j]!=0){
+                    if(matrix[0][j]==0 || matrix[i][0]==0){
+                        matrix[i][j]=0;
+                    }   
                 }
+
+            }
+        }
+        if(matrix[0][0]==0){
+            for(int j = 0; j < m; j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(cola==0){
+            for(int i = 0 ; i < n; i++){
+                matrix[i][0]=0;
             }
         }
     }

0bab8bc6735bc857377559b951e7ae33bac5b363
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2024-06-10 15:11:08 +0530

    Time: 4 ms (12.04%) | Memory: 7.6 MB (44.94%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
-   int nCr(int n, int r) {
-        if (r > n) return 0;
-        if (r == 0 || n == r) return 1;
-        double res = 0;
-        for (int i = 0; i < r; i++) {
-            res += log(n-i) - log(i+1);
-        }
-        return (int)round(exp(res));
+    int fun(int m, int n,vector<vector<int>>&dp){
+        if(m==0 and n==0)return 1;
+        if(n<0 or m<0)return 0;
+        if(dp[m][n]!=-1)return dp[m][n];
+        return dp[m][n]=fun(m-1,n,dp)+fun(m,n-1,dp);
     }
-
     int uniquePaths(int m, int n) {
-        int t = (m-1) + (n-1);
-       
-           return nCr(t,m-1);
-       
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return fun(m-1,n-1,dp);
     }
 };

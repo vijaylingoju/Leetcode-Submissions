3b5bbbfac3a850a5f95975dd4c1a17da7b41d8d2
Venkata Sai Naga Lakshmi Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2024-07-22 21:40:04 +0530

    Time: 634 ms (5.01%) | Memory: 295 MB (6.48%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int fun(int i, int prev, vector<int>&nums, vector<vector<int>>&dp){
-        if(i>=nums.size())return 0;
+    int fun(int i, int prev, vector<int>&nums, int n, vector<vector<int>>&dp){
+        if(i>=n)return 0;
         if(dp[i][prev+1]!=-1)return dp[i][prev+1];
-        int nopick=fun(i+1,prev,nums,dp);
-        int pick=INT_MIN;
+        int pick = INT_MIN;
         if(prev==-1 or nums[i]>nums[prev]){
-            pick=1+fun(i+1,i,nums,dp);
+            pick = 1 + fun(i+1,i,nums,n,dp);
         }
+        int nopick = fun(i+1,prev,nums,n,dp);
         return dp[i][prev+1]=max(pick,nopick);
     }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return fun(0,-1,nums,dp);
+        return fun(0,-1,nums,n,dp);
     }
 };

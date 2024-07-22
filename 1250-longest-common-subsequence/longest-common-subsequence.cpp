f6a5f1b56e23f69f3446bb5d0062498a98a71417
Venkata Sai Naga Lakshmi Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2024-07-22 21:55:10 +0530

    Time: 32 ms (35.87%) | Memory: 25.8 MB (41.69%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string t1, string t2) {
-        int m=t1.size(),n=t2.size();
-        int dp[m+1][n+1],i,j;
-        for(i=0;i<m+1;i++)
-        {
-            dp[i][0]=0;
+    int fun(int i, int j, string &a, string &b, vector<vector<int>>&dp){
+        if(i<0 or j<0)return 0;
+        int pick = INT_MIN;
+        int nopick = INT_MIN;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(a[i]==b[j]){
+            pick = 1 + fun(i-1,j-1,a,b,dp);
         }
-        for(i=1;i<n+1;i++)
-        {
-            dp[0][i]=0;
+        else{
+            nopick = max(fun(i-1,j,a,b,dp),fun(i,j-1,a,b,dp));
         }
-        for(i=1;i<m+1;i++)
-        {
-            for(j=1;j<n+1;j++)
-            {
-                
-                if(t1[i-1]==t2[j-1]) dp[i][j]=dp[i-1][j-1]+1;
-                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+        return dp[i][j]=max(pick,nopick);
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i = 0; i < n + 1; i++){
+            dp[i][0] = 0;
+        }
+        for(int i = 0; i < m + 1; i++){
+            dp[0][i] = 0;
+        }
+        for(int i = 1; i < n + 1; i++){
+            for(int j = 1; j < m + 1; j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
-        return dp[m][n];
+        return dp[n][m];
     }
 };

9195ad40db4f05741b2874cf1401b3ec25a8566c
Venkata Sai Naga Lakshmi Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2024-07-25 10:50:49 +0530

    Time: 39 ms (38.50%) | Memory: 12.7 MB (9.09%) - LeetSync

---
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int dp[105][105];
+    int dfs(vector<int>& cuts, int i, int j,vector<vector<int>>&dp) {
+        if (j - i <= 1) return 0;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int ans = 1e9;
+
+        for (int k = i + 1; k < j; k++) {
+            ans = min(ans, cuts[j] - cuts[i] + dfs(cuts, i, k,dp) + dfs(cuts, k, j,dp));
+        }
+        return dp[i][j] = ans;
+    }
+    
+    int minCost(int n, vector<int>& cuts) {
+        cuts.push_back(0);
+        cuts.push_back(n);
+        sort(cuts.begin(), cuts.end());
+        vector<vector<int>>dp(cuts.size()+1,vector<int>(cuts.size()+1,-1));
+        int ans = dfs(cuts, 0, cuts.size() - 1,dp);
+        // for(auto it:dp){
+        //     for(auto i:it){
+        //         cout<<i<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return ans;
+    }
+};

00ffbd54c329a297f7dd4d5cfc036d5349b2d890
Venkata Sai Naga Lakshmi Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2024-07-28 00:25:08 +0530

    Time: 3 ms (78.27%) | Memory: 14.1 MB (24.28%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -7,14 +7,12 @@ public:
             ss.insert(ds);
             return;
         }
-        if(i>=n)return;
-        int j = i+1;
-        while(j<n and nums[j]==nums[i])j++;
-        fun(j,n,nums,k,ds);
-
-        ds.push_back(nums[i]);
-        fun(i+1,n,nums,k-nums[i],ds);
-        ds.pop_back();
+        for(int j = i; j < n; j++){
+            if(j>i and nums[j]==nums[j-1])continue;
+            ds.push_back(nums[j]);
+            fun(j+1,n,nums,k-nums[j],ds);
+            ds.pop_back();
+        }
 
     }
     vector<vector<int>> combinationSum2(vector<int>& nums, int k) {

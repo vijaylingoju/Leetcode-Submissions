e0782b1c7402299c10c2a6d94c53f3d24599eadb
Venkata Sai Naga Lakshmi Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2024-11-02 16:50:31 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (96.30%) - LeetSync

---
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        int n = sentence.size();
+        for(int i = 0; i < n; i++){
+            if(sentence[i]==' '){
+                if(sentence[i-1]!=sentence[i+1]){
+                    return false;
+                }
+            }
+        }
+        if(sentence[0]!=sentence.back())return false;
+        return true;
+    }
+};
